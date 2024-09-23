ad24d13e4a99a0321621d98db512294efb7e9eed
Ethan Mcintyre <mcintyea@mail.uc.edu>
2024-09-23 09:45:07 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int numtoexamine = 2;
+        numtoexamine = numtoexamine % 2;
+        System.out.println("The number entered is " + numtoexamine);
+
+    }
+
+}
+
+
